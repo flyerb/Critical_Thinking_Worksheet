97e32a0e5145d02f3be6243e719b6228fcf53b98
FlyerB <zarandazchi@gmail.com>
2020-04-26 11:56:58 -0500

    For Loops

---
+++ Critical_Thinking_Worksheet/For Loop.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Critical_Thinking_Worksheet
+{
+    class For_Loop
+    {
+        public void ForLoops()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"The loop is on {i} .");
+                
+            }
+        }
+
+        public void BackwardsLoop()
+        {
+            for (int i = 9; i < 10 && i > 4; i--)
+            {
+                Console.WriteLine($"This loop is counting down. We are now on {i}");
+            }
+        }
+
+        public void UserChoice()
+        {
+            int userchoice = 10;
+            for(int i = 0; i < userchoice; i++)
+            {
+                Console.WriteLine("You are participating in a for loop! Please type a number to continue.");
+                userchoice = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public void StringOfLetters()
+        {
+            string example = "Hello World";
+
+            for(int i = 0; i < example.Length; i++)
+            {
+                Console.WriteLine(example[i]);
+            }
+        }
+
+        public void EveryOtherLetter()
+        {
+            string example = "Hello World";
+
+            for(int i =0; i < example.Length; i++)
+            {
+                Console.WriteLine(example.Substring(1, 1));
+            }
+        }
+
+        public void DivisibleByThree()
+        {
+            string example = "Hello World";
+
+            for(int i = 0; i <example.Length; i++)
+            {
+                Console.WriteLine(example[i % 3]);
+            }
+        }
+
+
+    }
+}
---
+++ Critical_Thinking_Worksheet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Critical_Thinking_Worksheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            For_Loop example = new For_Loop();
+            //example.ForLoops();
+            // example.BackwardsLoop();
+            //example.UserChoice();
+            //example.StringOfLetters();
+            //example.EveryOtherLetter();
+            example.DivisibleByThree();
+        }
+    }
+}
